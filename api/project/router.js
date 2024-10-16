cdf94f383b31124888b3f7358f0faf2294418c25
Camden Hess <camden.hess12@gmail.com>
2024-10-16 12:11:07 -0600

    'server is running'

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+// const Project = require('./recipes-model')
+
+
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: 'somethings wrong inside the project router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
